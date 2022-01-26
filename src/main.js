ecf029101b7001e1f1a1e2ba28c38cc413511458
korealu <643949593@qq.com>
2022-01-26 11:59:58 +0800

    feat(view): version 1.0.0
    version 1.0.0

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2022-01-18 14:46:55
- * @LastEditTime: 2022-01-26 11:47:33
+ * @LastEditTime: 2022-01-26 11:59:17
  * @LastEditors: korealu
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /h5-active-v2/src/main.js
@@ -20,8 +20,6 @@ Vue.config.productionTip = false;
 import "normalize.css";
 import "animate.css";
 import "@/style/index.less";
-import Vconsole from "vconsole";
-new Vconsole();
 
 store.dispatch("getLocalStorageInit").then((r) => console.log(r));
 Vue.use(globalRegister);
--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const routes = [
     path: "/group",
     name: "Home",
     component: Home,
-  }
+  },
 ];
 
 const router = new VueRouter({
