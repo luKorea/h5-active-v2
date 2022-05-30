19c367ca6cac109eec008f2758718e06ddbc7195
korealu <643949593@qq.com>
2022-05-30 10:04:26 +0800

    endPage

--- src/views/endPage/index.vue
+++ src/views/endPage/index.vue
@@ -2,7 +2,7 @@
  * @Author: korealu 643949593@qq.com
  * @Date: 2022-04-20 09:35:06
  * @LastEditors: korealu 643949593@qq.com
- * @LastEditTime: 2022-05-30 09:52:08
+ * @LastEditTime: 2022-05-30 09:58:51
  * @FilePath: /h5-active-v2/src/views/endPage/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
