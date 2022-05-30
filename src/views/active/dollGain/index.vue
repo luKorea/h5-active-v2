dc58349c53a2155166d08d0a8ac878c9dbc1d91b
korealu <643949593@qq.com>
2022-05-30 15:11:51 +0800

    fix(active): 修改限定人偶静态页面
    修改限定人偶静态页面

--- src/views/active/dollGain/index.vue
+++ src/views/active/dollGain/index.vue
@@ -2,7 +2,7 @@
  * @Author: korealu 643949593@qq.com
  * @Date: 2022-05-30 11:15:40
  * @LastEditors: korealu 643949593@qq.com
- * @LastEditTime: 2022-05-30 14:42:03
+ * @LastEditTime: 2022-05-30 15:09:19
  * @FilePath: /h5-active-v2/src/views/active/functionSubscription/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
