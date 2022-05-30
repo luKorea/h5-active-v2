48510c9711d416fa5654b3cc154c8eaa6c59d4b1
korealu <643949593@qq.com>
2022-05-30 11:57:46 +0800

    fix(active): 修改其余页面emit时间
    修改其余页面emit事件

--- src/views/active/rechargeDiscounts/index.vue
+++ src/views/active/rechargeDiscounts/index.vue
@@ -2,7 +2,7 @@
  * @Author: korealu 643949593@qq.com
  * @Date: 2022-05-30 11:17:44
  * @LastEditors: korealu 643949593@qq.com
- * @LastEditTime: 2022-05-30 11:17:44
+ * @LastEditTime: 2022-05-30 11:56:35
  * @FilePath: /h5-active-v2/src/views/active/rechargeDiscounts/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -21,6 +21,14 @@
 <script>
 export default {
   name: "activeRechargeDiscountsPage",
+  methods: {
+    handleLoginDialog() {
+      this.$emit("handleLoginDialog");
+    },
+    handlePayDialog() {
+      this.$emit("handlePayDialog");
+    },
+  },
 };
 </script>
 
