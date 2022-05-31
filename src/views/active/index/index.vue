f11d61625e3ac14463c64d270138860d89be51a7
korealu <643949593@qq.com>
2022-05-31 09:15:24 +0800

    fix(nothing): 办公前提交一次
    办公前提交一次

--- src/views/active/index/index.vue
+++ src/views/active/index/index.vue
@@ -2,7 +2,7 @@
  * @Author: korealu 643949593@qq.com
  * @Date: 2022-05-30 10:50:55
  * @LastEditors: korealu 643949593@qq.com
- * @LastEditTime: 2022-05-30 17:21:10
+ * @LastEditTime: 2022-05-31 09:14:37
  * @FilePath: /h5-active-v2/src/views/active/index/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
