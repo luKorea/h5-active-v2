2c5ba0a49757d78a47df36066157d36251557ee3
korealu <643949593@qq.com>
2022-06-08 17:10:24 +0800

    fix(618活动): 618活动一些bug修改
    618活动一些bug修改

--- src/views/active/dollGain/index.vue
+++ src/views/active/dollGain/index.vue
@@ -2,7 +2,7 @@
  * @Author: korealu 643949593@qq.com
  * @Date: 2022-05-30 11:15:40
  * @LastEditors: korealu 643949593@qq.com
- * @LastEditTime: 2022-06-08 16:56:35
+ * @LastEditTime: 2022-06-08 17:06:20
  * @FilePath: /h5-active-v2/src/views/active/functionSubscription/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -36,7 +36,7 @@ recordList：组队用户信息
     <div class="get-wrap">
       <img :src="imgInfo.bannerImg" alt="" referrerpolicy="no-referrer" />
       <template v-if="otherInfo && !otherInfo.have">
-        <template v-if="!uid || otherInfo.vote">
+        <template v-if="!$route.query.inviteCode && (!uid || otherInfo.vote)">
           <!-- 未登录以及未拥有人偶展示 -->
           <div class="init-wrap">
             <div class="btn bug" @click="handleDifferentOperation('buy')">
