8c4c803e3142d81dd5cd117ae1fd861ee734003a
korealu <643949593@qq.com>
2022-07-06 11:15:45 +0800

    fix(summervacation): summerVacation/修改store名字
    summerVacation/修改store名字

--- src/views/summerVacation/index.vue
+++ src/views/summerVacation/index.vue
@@ -2,7 +2,7 @@
  * @Author: korealu 643949593@qq.com
  * @Date: 2022-07-06 10:38:33
  * @LastEditors: korealu 643949593@qq.com
- * @LastEditTime: 2022-07-06 11:00:49
+ * @LastEditTime: 2022-07-06 11:14:43
  * @FilePath: /h5-active-v2/src/views/summerVacation/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -121,7 +121,7 @@ export default {
       const phoneStr = tel.substr(0, 3) + "****" + tel.substr(7);
       localCache.setCache("phoneStr", phoneStr);
       this.$store
-        .dispatch("activeModule/loginAction", data)
+        .dispatch("summaryModule/loginAction", data)
         .then(() => {
           successInfo("登录成功");
           _this.showLoginDialog = false;
@@ -130,7 +130,7 @@ export default {
         .catch((err) => errorInfo(err));
     },
     logout() {
-      this.$store.dispatch("activeModule/logoutAction").then(() => {
+      this.$store.dispatch("summaryModule/logoutAction").then(() => {
         // this.$router.replace("/active");
         this.$router.go(0);
       });
